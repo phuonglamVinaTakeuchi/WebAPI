7f98c5e8dd64edf5e607a99e74488ad91b07b37f
Lam Nguyen <Lam Nguyen@DESKTOP-HC251IR>
2021-06-11 09:20:18 +0700

    Add project files.

---
+++ UsingAPI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace UsingAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RunAsync().Wait();
+            Console.ReadKey();
+        }
+
+        static async Task RunAsync()
+        {
+            using var client = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:5000")
+            };
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("/weatherforecast");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsAsync<WeatherForecast[]>();
+                Console.WriteLine($"Records retrieved: {data.Length}");
+                foreach (var d in data)
+                {
+                    Console.WriteLine($"{d.Date}\t{d.TemperatureC}\t\t{d.Summary}");
+                }
+            }
+        }
+    }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public string Summary { get; set; }
+    }
+}
---
+++ WebAPI/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+        IEnumerable<string> _presidents = new List<string> {
+                "Biden", "Trump", "Bush", "Clinton"
+            };
+
+        public IEnumerable<string> Index()
+        {
+            return _presidents;
+        }
+    }
+}
+
